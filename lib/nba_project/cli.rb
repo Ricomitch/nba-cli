9d306f9cd450782ac6255ff93dde9a5c1529bc87
Rico Mitchell <ricomitch@icloud.com>
2020-03-02 05:51:48 +0000

    add class to cli file.

--- lib/nba_project/cli.rb
+++ lib/nba_project/cli.rb
@@ -1,8 +1,6 @@
-require "nba/cli/version"
-
-module Nba
-  module Cli
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
+class nba_cli :: class
+  
+  def start
+    puts "Hello there!"
+  end  
+end  
